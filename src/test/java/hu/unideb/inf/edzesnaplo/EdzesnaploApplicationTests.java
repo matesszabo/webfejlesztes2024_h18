731bffec0398de8254792118d3ca31249cb26a4b
matesszabo <matesszabo@hotmail.com>
2024-10-07 19:05:03 +0200

    project init

---
+++ src/test/java/hu/unideb/inf/edzesnaplo/EdzesnaploApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.edzesnaplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdzesnaploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
