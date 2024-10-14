0e85f60c7e074c8dec0c7cd8e477c633e511babe
matesszabo <matesszabo@hotmail.com>
2024-10-14 18:59:25 +0200

    felhasnaloEntity

---
+++ src/main/java/hu/unideb/inf/edzesnaplo/data/entity/FelhasznaloEntity.java
@@ -0,0 +1,120 @@
+package hu.unideb.inf.edzesnaplo.data.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "felhasznalo")
+public class FelhasznaloEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "nev")
+    private String nev;
+    @Column(name = "szuletesiDatum")
+    private Date szuletesiDatum;
+    @Column(name = "suly")
+    private Float suly;
+    @Column(name = "magassag")
+    private Integer magassag;
+    @Column(name = "jelszo")
+    private String jelszo;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "cel")
+    private String cel;
+
+    public FelhasznaloEntity() {
+    }
+
+    public FelhasznaloEntity(long id, String nev, Date szuletesiDatum, Float suly, Integer magassag, String jelszo, String email, String cel) {
+        this.id = id;
+        this.nev = nev;
+        this.szuletesiDatum = szuletesiDatum;
+        this.suly = suly;
+        this.magassag = magassag;
+        this.jelszo = jelszo;
+        this.email = email;
+        this.cel = cel;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public Date getSzuletesiDatum() {
+        return szuletesiDatum;
+    }
+
+    public void setSzuletesiDatum(Date szuletesiDatum) {
+        this.szuletesiDatum = szuletesiDatum;
+    }
+
+    public Float getSuly() {
+        return suly;
+    }
+
+    public void setSuly(Float suly) {
+        this.suly = suly;
+    }
+
+    public Integer getMagassag() {
+        return magassag;
+    }
+
+    public void setMagassag(Integer magassag) {
+        this.magassag = magassag;
+    }
+
+    public String getJelszo() {
+        return jelszo;
+    }
+
+    public void setJelszo(String jelszo) {
+        this.jelszo = jelszo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCel() {
+        return cel;
+    }
+
+    public void setCel(String cel) {
+        this.cel = cel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FelhasznaloEntity that = (FelhasznaloEntity) o;
+        return id == that.id && Objects.equals(nev, that.nev) && Objects.equals(szuletesiDatum, that.szuletesiDatum) && Objects.equals(suly, that.suly) && Objects.equals(magassag, that.magassag) && Objects.equals(jelszo, that.jelszo) && Objects.equals(email, that.email) && Objects.equals(cel, that.cel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nev, szuletesiDatum, suly, magassag, jelszo, email, cel);
+    }
+}
