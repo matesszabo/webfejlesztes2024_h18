709b792573e39f4dc5850546660bf353a3ed14c8
matesszabo <matesszabo@hotmail.com>
2024-10-28 18:35:55 +0100

    Create, update etel

--- src/main/java/hu/unideb/inf/edzesnaplo/controller/EtelController.java
+++ src/main/java/hu/unideb/inf/edzesnaplo/controller/EtelController.java
@@ -1,14 +1,18 @@
 package hu.unideb.inf.edzesnaplo.controller;
 
+import hu.unideb.inf.edzesnaplo.data.entity.EtelEntity;
+import hu.unideb.inf.edzesnaplo.data.repository.EtelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController//json-t küld, json-t fogad
 @RequestMapping("/api")
 public class EtelController {
+
+    @Autowired
+    EtelRepository repository;
 
     @GetMapping("/hello")
     public String hello(){
@@ -18,6 +22,20 @@ public class EtelController {
     @GetMapping("/hw")
     public ResponseEntity<String> helloworld(){
         return new ResponseEntity<>("Hello, world!", HttpStatus.NOT_FOUND);
+    }
+
+    //localhost:8080/api/saveetel POST
+    @PostMapping("/saveetel")
+    public EtelEntity save(@RequestBody EtelEntity etel){
+        return repository.save(etel);
+    }
+
+    @PutMapping("/updateetel")
+    public EtelEntity update(@RequestBody EtelEntity etel){
+        if(etel.getId() > 0L){
+            return repository.save(etel);
+        }
+        return null;
     }
 }
 
