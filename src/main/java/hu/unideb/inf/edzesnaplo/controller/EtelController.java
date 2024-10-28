1b72b875da24e984a0e7a2a3772daa381006cadd
matesszabo <matesszabo@hotmail.com>
2024-10-28 19:01:45 +0100

    etel by nev

--- src/main/java/hu/unideb/inf/edzesnaplo/controller/EtelController.java
+++ src/main/java/hu/unideb/inf/edzesnaplo/controller/EtelController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController//json-t küld, json-t fogad
@@ -51,6 +52,27 @@ public class EtelController {
     public List<EtelEntity> findAll(){
         return repository.findAll();
     }
+
+    //localhost:8080/api/etel/rizs GET
+    @GetMapping("/etel/{nev}")
+    public List<EtelEntity> findAllByNev(@PathVariable String nev){
+        List<EtelEntity> szurt = new ArrayList<>();
+
+        szurt = repository.findAll()
+                .stream()
+                .filter(x -> x.getNev().contains(nev))
+                .toList();
+
+        return szurt;
+    }
+
+    //localhost:8080/api/etelbynev?nev=rizs GET
+    @GetMapping("/etelbynev")
+    public List<EtelEntity> findAllByNevRp(@RequestParam String nev){
+        return repository.findAllByNevContains(nev);
+    }
+
+
 }
 
 
--- src/main/java/hu/unideb/inf/edzesnaplo/data/repository/EtelRepository.java
+++ src/main/java/hu/unideb/inf/edzesnaplo/data/repository/EtelRepository.java
@@ -4,6 +4,11 @@ import hu.unideb.inf.edzesnaplo.data.entity.EtelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EtelRepository extends JpaRepository<EtelEntity, Long> {
+
+    //select * from Etel where nev like '%nev%'
+    List<EtelEntity> findAllByNevContains(String nev);
 }
