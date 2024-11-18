c58aadf3c33fd8bab4a7da2107c31370da3309c6
matesszabo <matesszabo@hotmail.com>
2024-11-18 19:20:28 +0100

    virtual threads enable

--- src/main/java/hu/unideb/inf/edzesnaplo/controller/AuthController.java
+++ src/main/java/hu/unideb/inf/edzesnaplo/controller/AuthController.java
@@ -4,10 +4,7 @@ import hu.unideb.inf.edzesnaplo.service.AuthService;
 import hu.unideb.inf.edzesnaplo.service.dto.BejelentkezesDto;
 import hu.unideb.inf.edzesnaplo.service.dto.RegisztracioDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,5 +20,10 @@ public class AuthController {
     @PostMapping("/bejelentkezes")
     public String bejelentkezes(@RequestBody BejelentkezesDto dto){
         return authService.bejelentkezes(dto);
+    }
+
+    @GetMapping("/vt")
+    public String vt(){
+        return Thread.currentThread().toString();
     }
 }
